0795f0d3325224409adc7e4a101a16a3f3f5385f
Tobias Schoch <Tobias.Schoch@helbling.ch>
2019-04-09 10:09:37 +0200

    created a separate module per repo server

---
+++ gittools/reposervers/github.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+# created: 09.04.2019
+# author:  TOS
+
+import logging
+import urllib
+import github
+
+from .reposerver import RepoServer
+
+log = logging.getLogger(__name__)
+
+
+class Github(RepoServer):
+
+    def __init__(self, name, url="https://github.com/", **kwargs):
+
+        self.name = name
+        self.url = urllib.parse.urlparse(url)
+
+        self.login_info = {}
+        self.repo_info = {}
+
+        self.ssh = kwargs.get('ssh', False)
+        self.repo_info.update(kwargs.get('newrepo', {}))
+        self.login_info.update(kwargs.get('login', {}))
+
+        # First create a Github instance:
+        if not 'Token' in self.login_info:
+            self.username = self.login_info['Username']
+            self.password = self.login_info['Password']
+
+            # using username and password
+            self.api = github.Github(self.username, self.password)
+        else:
+            self.token = self.login_info['Token']
+
+            self.api = github.Github(self.token)
+
+        self.user = self.api.get_user()
+
+    def create_repository(self, name, description, **info):
+        repo = self.user.create_repo(name=name, description=description, **info)
+        if self.ssh:
+            return self.Repo(repo.name, repo.id, repo.ssh_url)
+        else:
+            return self.Repo(repo.name, repo.id, repo.clone_url)
+
+    def delete_repository(self, name):
+        repo = self.user.get_repo(name)
+        repo.delete()
+
+    def get_repository(self, name):
+        repo = self.user.get_repo(name)
+        if self.ssh:
+            return self.Repo(repo.name, repo.id, repo.ssh_url)
+        else:
+            return self.Repo(repo.name, repo.id, repo.clone_url)
+
+    def repository_exists(self, name):
+        try:
+            self.user.get_repo(name)
+            return True
+        except:
+            return False
---
+++ gittools/reposervers/reposerver.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+# created: 05.03.2018
+# author:  TOS
+
+import logging
+import importlib
+
+from ..config import get_cfg
+
+log = logging.getLogger(__name__)
+
+
+def repository_servers_cfg():
+    return get_cfg()['reposervers'].copy()
+
+
+class RepoServer(object):
+    class Repo(object):
+
+        def __init__(self, name, id, giturl):
+            self.id = id
+            self.name = name
+            self.giturl = giturl
+
+    def __init__(self, name, url, **kwargs):
+        self.username = None
+        self.password = None
+        self.name = None
+        raise NotImplementedError()
+
+    def create_repository(self, name, description, **info):
+        raise NotImplementedError()
+
+    def delete_repository(self, name):
+        raise NotImplementedError()
+
+    def get_repository(self, name) -> Repo:
+        raise NotImplementedError()
+
+    def repository_exists(self, name):
+        raise NotImplementedError()
+
+
+def get_repo_server(name) -> RepoServer:
+    cfg = repository_servers_cfg()
+
+    srv_cfg = cfg[name].copy()
+    srv_cfg['name'] = name
+    repo_type = srv_cfg.pop('type')
+    importlib.import_module(repo_type.lower())
+    srv_type = globals().get(repo_type)
+
+    srv = srv_type(**srv_cfg)
+    return srv
